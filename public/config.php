065474c6efc11c0d84f5a3dd941189fc46649bc7
Miguel Albrecht <malbrecht@zapalote.com>
2021-12-11 17:50:32 +0100

    v1.0 documentation

--- public/config.php
+++ public/config.php
@@ -4,8 +4,8 @@ date_default_timezone_set ("Europe/Amsterdam");
 setlocale(LC_TIME, 'en_NL');
 
 // App wide constants, these will get overwritten
-define('STATS_DB_DEV_INI', "../../private/inspira_stats.ini");
-define('STATS_DB_PROD_INI', "../../inspira.ini");
+define('STATS_DB_DEV_INI', "../../private/app-event.ini");
+define('STATS_DB_PROD_INI', "../../app-events.ini");
 define('STATS_SITE', "inspiratree.com");
 
 // Don't change beyond here
