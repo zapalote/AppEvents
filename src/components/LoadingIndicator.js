9098532fecf92686b6bef48994b117189c0921b6
Miguel Albrecht <malbrecht@zapalote.com>
2021-12-27 11:28:26 +0100

    streamlined svg

--- src/components/LoadingIndicator.js
+++ src/components/LoadingIndicator.js
@@ -1,18 +1,37 @@
-import * as SVGLoaders from 'svg-loaders-react';
+// from https://github.com/ajwann/svg-loaders-react
 
 const LoadingIndicator = (props) => {
 
-    const { width = 40, coverall = true, fill = '#fff' } = props;
+    const { width = 60, coverall = true, fill = '#fff', className = '' } = props;
     return ( 
         <div style={Object.assign({}, spinner, coverall && overlay)}>
-           <SVGLoaders.Grid width={width} fill={fill}/>
+          <svg width={width} height={width} stroke="#000" viewBox="0 0 60 60" fill={fill} className={className} >
+            <g
+              transform="translate(1 1)"
+              strokeWidth={2}
+              fill="none"
+              fillRule="evenodd"
+            >
+              <circle strokeOpacity={0.5} cx={18} cy={18} r={18} />
+              <path d="M36 18c0-9.94-8.06-18-18-18">
+                <animateTransform
+                  attributeName="transform"
+                  type="rotate"
+                  from="0 18 18"
+                  to="360 18 18"
+                  dur="1s"
+                  repeatCount="indefinite"
+                />
+              </path>
+            </g>
+          </svg>
         </div>
     );
   };
 
   const spinner = {
     width: "100%",
-    height: "100%",
+    height: "10rem",
     display: "flex",
     justifyContent: "center",
     alignItems: "center",
